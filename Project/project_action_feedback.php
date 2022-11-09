446bffc7ff4572be2bab7d42479f7870d4ad9023
Ereb0Z <116702816+Ereb0Z@users.noreply.github.com>
2022-11-09 22:25:01 +0700

    Update project_action_feedback.php

--- Project/project_action_feedback.php
+++ Project/project_action_feedback.php
@@ -2,6 +2,10 @@
 
     $nama = $_POST['nama']; 
 
+    $email = $_POST['email'];
+
+
+
 ?> 
 
 <html> 
@@ -10,12 +14,32 @@
     
         <!--link css-->
         <link rel="stylesheet" href="style_homepage.css">
-        </head> 
 
+         <!--Link Font Font Awesome-->
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    
+        <!-- Link Box Icon -->
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+        <!-- Link Google Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+
+        </head> 
+        <body>
+        <nav class="navbar item">
+        <i class='bx bx-menu icon'></i>
+        
+        <ul class="menu">
+            <li><a href="Project_homepage.html">Home</a></li>
+        </ul>
+    </nav>
         <hr>
         <br>
         <?php 
-            echo "<p>Terima kasih ".$nama. " telah memberikan masukan </p>";
+            echo "<p>Terima kasih ".$nama. " telah memberikan masukan. </p>";
+
         ?>
         <br>
         <hr>
+ 
